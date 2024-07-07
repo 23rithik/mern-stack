0bc573b93a680f534b345ffbafdc90c5592a8f46
23rithik <chithrarithik@gmail.com>
2024-07-07 17:39:56 +0530

    added get

--- Employeeappbackend/app.js
+++ Employeeappbackend/app.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use('/api', postRoute);
 app.use('/user', userRoute);
 
+app.get('/*',function(req,res){res.sendFile(path.join(__dirname,'/index.html'));});
 // Error Handling Middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
